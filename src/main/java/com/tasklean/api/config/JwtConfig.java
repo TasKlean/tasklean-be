7ccf788f5644faa985c223ba2146a11ace472254
neratmatevz <matevz.nerat@gmail.com>
2026-07-03 18:03:20 +0200

    chore: added auth dependencies and setup auth

--- src/main/java/com/tasklean/api/config/JwtConfig.java
+++ src/main/java/com/tasklean/api/config/JwtConfig.java
@@ -1,4 +1,16 @@
 package com.tasklean.api.config;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "jwt")
+@Getter
+@Setter
 public class JwtConfig {
+
+    private String secret;
+    private long expiration;
 }
